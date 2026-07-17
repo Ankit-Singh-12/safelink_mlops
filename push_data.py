f3f66ade2a7044b4d9901cc12fc795e1af5230e0
Ankit <singhankit.as777@gmail.com>
2026-07-18 03:27:04 +0530

    Added mongodb setup

---
+++ push_data.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import json
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
+import certifi
+ca=certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def csv_to_json(self, csv_file_path):
+        try:
+            data=pd.read_csv(csv_file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def insert_data_to_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+            
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database=self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return (len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+if __name__=='__main__':
+    FILE_PATH=os.path.join(os.getcwd(),"Network_Data","phisingData.csv")
+    DATABASE="SafeLink_db"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json(FILE_PATH)
+    no_of_records=networkobj.insert_data_to_mongodb(records,DATABASE,Collection)
+    print(f"Total {no_of_records} records inserted successfully into the database {DATABASE} and collection {Collection}")
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()-> List[str]:
+    """
+    Thiss function will return list of requirements
+    
+    """
+    try:
+        requirement_list=[]
+        with open("requirements.txt", "r") as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!= '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        raise FileNotFoundError("requirements.txt file not found.")
+    return requirement_list
+
+setup(
+    name="SafeLink",
+    version="0.0.1",
+    author="Ankit Singh",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
